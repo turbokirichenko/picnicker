551ce7682bbb05976835c0854e44f44798bbd4df
turbokirichenko <turbokirichenko@outlook.com>
2022-10-24 04:08:21 -0800

    genesis commit

---
+++ src/app/App.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<main>
+  <h1 class=".hcard">Hcard club genesis</h1>
+</main>
+
+<style lang="scss">
+  .heading {
+    color: tomato;
+    font-weight: 700;
+    font-size: 2em;
+    font-family: "Courier New";
+  }
+</style>
