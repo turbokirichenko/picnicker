6057a809a22e6263dcd1793f724fe14bdd133b9b
turbokirichenko <turbokirichenko@outlook.com>
2022-12-05 08:15:27 -0800

    start develop the target page

--- src/pages/greeting/index.svelte
+++ src/pages/greeting/index.svelte
@@ -1,3 +1,15 @@
-<main>
-  <div>Welcome!</div>
+<main class="main-page">
+  <div class="main-page__title">Welcome!</div>
 </main>
+
+<style lang="scss">
+  .main-page {
+    &__title {
+      text-align: center;
+      font-family: "Impact";
+      font-size: 48px;
+      margin: 30px 0;
+      line-height: 48px;
+    }
+  }
+</style>
