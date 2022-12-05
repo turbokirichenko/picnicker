60e249b6c9ddcd834c60ce68878f8fccc29da06e
turbokirichenko <turbokirichenko@outlook.com>
2022-12-05 01:53:35 -0800

    create features design

--- src/pages/greeting/index.svelte
+++ src/pages/greeting/index.svelte
@@ -1,11 +1,3 @@
 <main>
-  <h1>
-    <!-- svelte-ignore security-anchor-rel-noreferrer -->
-    Just create your professional
-    <a href="http://microformats.org/wiki/hcard" target="_blank">Hcard</a>
-  </h1>
-  <button />
+  <div>Welcome!</div>
 </main>
-
-<style lang="scss">
-</style>
