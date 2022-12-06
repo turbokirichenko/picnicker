f672da195cfeac46e65709a04ff3120ac9c31551
turbokirichenko <turbokirichenko@outlook.com>
2022-12-06 09:35:30 -0800

    add animated blur layout

---
+++ src/features/blur-layout.svelte
@@ -0,0 +1,143 @@
+<script>
+  import { classes } from "~/shared/utils/classes";
+</script>
+
+<div class="blur-animated-block">
+  <div
+    class={classes(
+      "blur-animated-block__item",
+      "blur-animated-block__item-big",
+      "blur-animated-block__item-1"
+    )}
+  />
+  <div
+    class={classes(
+      "blur-animated-block__item",
+      "blur-animated-block__item-medium",
+      "blur-animated-block__item-2"
+    )}
+  />
+  <div
+    class={classes(
+      "blur-animated-block__item",
+      "blur-animated-block__item-medium",
+      "blur-animated-block__item-3"
+    )}
+  />
+  <div
+    class={classes(
+      "blur-animated-block__item",
+      "blur-animated-block__item-tiny",
+      "blur-animated-block__item-4"
+    )}
+  />
+  <div
+    class={classes(
+      "blur-animated-block__item",
+      "blur-animated-block__item-tiny",
+      "blur-animated-block__item-5"
+    )}
+  />
+  <div class="blur-animated-block__blur">
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  .blur-animated-block {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    background-color: #98329f80;
+    top: 0;
+    left: 0;
+
+    &__item {
+      position: absolute;
+      background-color: #98329f;
+      display: block;
+      border-radius: 50%;
+      animation-name: rotate;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      filter: blur(10px);
+      z-index: -1;
+
+      &-1 {
+        top: 10%;
+        left: 20%;
+        animation-delay: -2s;
+        animation-direction: reverse;
+        animation-duration: 40s;
+      }
+
+      &-2 {
+        bottom: 10%;
+        right: 0%;
+        animation-delay: -4s;
+        animation-duration: 25s;
+      }
+
+      &-5 {
+        top: 70%;
+        left: 35%;
+        animation-delay: -10s;
+        animation-duration: 20s;
+      }
+
+      &-4 {
+        top: 20%;
+        right: 5%;
+        animation-delay: -8s;
+        animation-direction: reverse;
+        animation-duration: 30s;
+      }
+
+      &-3 {
+        bottom: 50%;
+        right: -5%;
+        animation-delay: -6s;
+        animation-duration: 15s;
+      }
+
+      &-big {
+        width: 20vmax;
+        height: 20vmax;
+      }
+
+      &-medium {
+        width: 18vmax;
+        height: 18vmax;
+      }
+
+      &-tiny {
+        width: 14vmax;
+        height: 14vmax;
+      }
+    }
+
+    &__blur {
+      display: block;
+      max-width: 900px;
+      overflow: hidden;
+      margin: auto;
+    }
+  }
+
+  @keyframes rotate {
+    0% {
+      transform: rotate(0deg) translate(100%, 0);
+      filter: blur(10px) opacity(1);
+    }
+
+    50% {
+      transform: rotate(180deg) translate(100%, 0) scale(0.7);
+      filter: blur(20px) opacity(0.5);
+    }
+
+    100% {
+      transform: rotate(360deg) translate(100%, 0);
+      filter: blur(10px) opacity(1);
+    }
+  }
+</style>
--- src/pages/greeting/index.svelte
+++ src/pages/greeting/index.svelte
@@ -1,15 +1,53 @@
-<main class="main-page">
-  <div class="main-page__title">Welcome!</div>
+<script>
+  import BlurLayout from "~/features/blur-layout.svelte";
+</script>
+
+<main class="greeting-page">
+  <div class="greeting-page__fixed-item">
+    <BlurLayout>
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    </BlurLayout>
+  </div>
+  <div class="greeting-page__floated-item">
+    <div class="internal-block" />
+  </div>
 </main>
 
 <style lang="scss">
-  .main-page {
-    &__title {
-      text-align: center;
-      font-family: "Impact";
-      font-size: 48px;
-      margin: 30px 0;
-      line-height: 48px;
+  .greeting-page {
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+
+    &__fixed-item {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 101vh;
+      z-index: -1;
+      overflow: hidden;
     }
+
+    &__floated-item {
+      position: relative;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      min-height: 400px;
+      margin-top: 100vh;
+      padding-top: 80px;
+
+      overflow: hidden;
+    }
+  }
+
+  .internal-block {
+    width: 100%;
+    height: 100%;
+    background-color: mediumpurple;
   }
 </style>
