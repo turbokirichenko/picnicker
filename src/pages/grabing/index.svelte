b04e3fd33cf856dfcc662541a1271f20fd46c6dc
turbokirichenko <turbokirichenko@outlook.com>
2022-12-12 03:15:00 -0800

    start develop grabing page

---
+++ src/pages/grabing/index.svelte
@@ -0,0 +1,21 @@
+<script>
+  let loading = true;
+</script>
+
+<div class="grabing-page">
+  {#if loading}
+    <div class="grabing-page__loading-theme">
+      <div class="loading-layout">loading</div>
+    </div>
+  {:else}
+    <div class="grabing-page__profile">
+      <div class="user-profile">
+        <div class="user-profile__header">header</div>
+        <div class="user-profile__contacts">items</div>
+      </div>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+</style>
