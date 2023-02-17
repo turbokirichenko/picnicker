9135f69b231e12a4c30f593da00859ba8d1ad0df
turbokirichenko <turbokirichenko@outlook.com>
2023-02-17 14:56:36 -0800

    fix bug

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,4 +1,4 @@
-export const DOMAIN_NAME = 'pinicker.netlify.app'
+export const DOMAIN_NAME = 'picnicker.netlify.app'
 export const FULL_SHOW_PATH = 'https://picnicker.netlify.app/#/'
 export const PHONE_PATTERN = /(\+{0,})(\d{0,})([(]{1}\d{1,3}[)]{0,}){0,}(\s?\d+|\+\d{2,3}\s{1}\d+|\d+){1}[\s|-]?\d+([\s|-]?\d+){1,2}(\s){0,}/;
 export const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
