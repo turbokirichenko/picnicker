926947239bb101e79e4dcdc4a030028003e43b21
turbokirichenko <turbokirichenko@outlook.com>
2023-02-18 06:22:21 -0800

    create main page

--- src/pages/greeting/index.svelte
+++ src/pages/greeting/index.svelte
@@ -1,11 +1,10 @@
 <script>
+  import { fade } from "svelte/transition";
   import { link } from "svelte-spa-router";
-  import { state } from "./storage";
-  import LogoGrablee from "~/shared/icons/logograblee.svg";
-
-  import SuperSearch from "~/widgets/super-search/index.svelte";
-  import ThemeButtom from "~/widgets/theme-button/index.svelte";
   import BlurBackground from "~/features/blur-layout.svelte";
+  import CircleBlock from "~/features/circle-block.svelte";
+  import FadeMenuBlock from "~/widgets/fade-menu-block.svelte";
+  import LoopSetInformation from "~/widgets/loop-set-information/index.svelte";
   import WallLayout from "~/features/wall-layout.svelte";
 
   let themeDark = true;
@@ -27,31 +26,66 @@
   };
 </script>
 
-<main class="greeting-page">
+<main class="greeting-page" out:fade={{ duration: 500 }}>
   <section class="greeting-page__background">
     <BlurBackground />
   </section>
   <section class="greeting-page__content-page">
     <div class="page-internal-content">
       <div class="page-internal-content__fixed-item">
-        <div class="description-block">
-          <div class="description-block__header">
-            <div class="header-nav-panel">
-              <ThemeButtom bind:dark={themeDark} hide={!elapsedSearch} />
-              <SuperSearch bind:elapsed={elapsedSearch} />
-            </div>
-          </div>
-          <div class="description-block__content">
-            <img class="logo" src={LogoGrablee} alt="logo white" />
-          </div>
-          <div class="description-block__footer">
-            <h3 class="footer-description">about project</h3>
-          </div>
-        </div>
+        <FadeMenuBlock />
       </div>
       <div class="page-internal-content__float-item">
         <WallLayout>
-          <div class="wall-content" />
+          <div class="wall-content">
+            <div class="wall-content__block-1">
+              <LoopSetInformation />
+            </div>
+            <div class="wall-content__block-2">
+              <div class="main-circle">
+                <CircleBlock
+                  title="HCARD philosophy"
+                  text="make a card in five seconds, and save it as.vcf file"
+                />
+              </div>
+            </div>
+            <div class="wall-content__block-3">
+              <div class="stepnstep">
+                <div class="stepnstep__block-1">
+                  <CircleBlock
+                    title="STEP 1"
+                    text="choice contact pattern, enter information about yourself"
+                    bg={true}
+                  />
+                </div>
+                <div class="stepnstep__block-2">
+                  <CircleBlock
+                    title="STEP 2"
+                    text="creating QR and take a screenshot!"
+                    bg={true}
+                  />
+                </div>
+                <div class="stepnstep__block-3">
+                  <CircleBlock
+                    title="STEP 3"
+                    text="Share with anything and anywhere you want!"
+                    bg={true}
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="wall-content__block-4">
+              <div class="contacts">
+                <span class="contact-text"
+                  >created by <a
+                    class="contact-text__link"
+                    href="https://github.com/turbokirichenko"
+                    >@turbokirichenko</a
+                  ></span
+                >
+              </div>
+            </div>
+          </div>
         </WallLayout>
       </div>
     </div>
@@ -92,203 +126,99 @@
       top: 100%;
       left: 0;
       width: 100%;
-      height: 1200px;
+      height: 2350px;
       overflow: hidden;
     }
-  }
-
-  .footer-description {
-    font-size: 21px;
-    font-family: Comfortaa;
-    text-align: center;
-    margin: 0;
-    color: #2e302f;
-    line-height: 60px;
-  }
-
-  .absolute-block {
-    position: absolute;
-    top: 0;
-    left: 0;
-    padding: 10px;
-    width: 100%;
-    height: 100%;
-  }
-
-  .description-block {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    position: relative;
-    top: 0;
-    left: 0;
-    &__header {
-      width: 100%;
-      max-width: 720px;
-      height: 80px;
-      min-height: 80px;
-      padding: 10px;
-      position: relative;
-    }
-    &__content {
-      max-width: 720px;
-      width: 40%;
-      min-width: 320px;
-      min-height: 360px;
-      padding: 20px;
-    }
-    &__footer {
-      max-width: 720px;
-      width: 100%;
-      height: 80px;
-      padding: 10px;
-    }
-  }
-
-  .header-nav-panel {
-    display: flex;
-    height: 100%;
-    justify-content: space-between;
-  }
-
-  .prologue-block {
-    width: 100%;
-    height: 100%;
-    color: #f9f9f9;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: column;
-    gap: 40px;
-    &__heading {
-      width: 100%;
-      height: 70px;
-    }
-    &__item {
-      width: 100%;
-      min-height: 120px;
-    }
-    &__toggle {
-      width: 100%;
-      height: 70px;
-      padding: 0 10px;
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-    }
-  }
-
-  .about-app-block {
-    width: 100%;
-    &__paragraph {
-      height: 60px;
-      font-size: 21px;
-      margin: 6px 0;
-      font-family: Comfortaa;
-      text-align: center;
-      overflow: hidden;
-    }
-  }
-
-  .medium-title {
-    margin: 0;
-    padding: 0;
-    font-size: 32px;
-    text-align: center;
-    line-height: 48px;
-    color: white;
-    font-family: Comfortaa;
-  }
-
-  .spantext-block {
-    text-align: center;
-    &__text {
-      font-family: Comfortaa;
-      font-size: 21px;
-      line-height: 28px;
-      text-align: center;
-    }
-    &__p {
-      padding: 10px;
-      font-size: 21px;
-      border: 2px solid #f9f9f9;
-      border-radius: 15px;
-    }
-  }
-
-  .arrow-button {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    gap: 10px;
-    top: 0;
-    left: 0;
-    padding: 20px;
-    border: 2px solid #f9f9f9;
-    border-radius: 40px;
-    padding: 0 10%;
-    background: none;
-    &__content {
-      height: 48px;
-    }
-    &__arrow {
-      position: absolute;
-      right: calc(10% - 10px);
-      width: 42px;
-      height: 100%;
-      transition: transform 0.5s;
-      transform: translate(0, 0);
-      display: flex;
-      justify-content: center;
-    }
-
-    &:hover {
-      .arrow-button__arrow {
-        transform: translate(10px, 0);
-      }
-    }
-  }
-
-  .arrow-img {
-    display: block;
-    width: 100%;
-    filter: invert(1);
   }
 
   .wall-content {
     width: 100%;
-    height: 100%;
+    height: calc(100% - 20px);
     background-color: #f9f9f9;
     border-radius: 30px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+
+    &__block-1 {
+      position: relative;
+      height: 560px;
+      padding-top: 80px;
+      margin-bottom: 20px;
+    }
+
+    &__block-2 {
+      width: 100%;
+      padding: 40px 0;
+      height: 560px;
+      position: relative;
+      top: 0;
+      left: 0;
+    }
+
+    &__block-3 {
+      width: 100%;
+      padding: 40px 0;
+      height: 1200px;
+      position: relative;
+      top: 0;
+      left: 0;
+    }
+
+    &__block-4 {
+      width: 100%;
+      padding: 20px;
+      height: 160px;
+      display: flex;
+      align-items: flex-end;
+      justify-content: center;
+      background-color: blueviolet;
+    }
+  }
+  .main-circle {
+    position: absolute;
+    width: 480px;
+    height: 480px;
+    top: calc(50% - 240px);
+    left: calc(50% - 240px);
   }
 
-  .logo {
+  .stepnstep {
+    position: relative;
     width: 100%;
-    display: block;
-    margin: auto;
+    height: 100%;
+    top: 0;
+    left: 0;
+    &__block-1 {
+      position: absolute;
+      width: 480px;
+      height: 480px;
+      top: calc(0);
+      left: calc(50% - 240px);
+    }
+    &__block-2 {
+      position: absolute;
+      width: 480px;
+      height: 480px;
+      top: 320px;
+      left: calc(50% - 240px);
+    }
+    &__block-3 {
+      position: absolute;
+      width: 480px;
+      height: 480px;
+      top: 640px;
+      left: calc(50% - 240px);
+    }
   }
 
-  @keyframes auto-change-theme {
-    0% {
-      background-color: rgb(255, 255, 255);
-    }
-    49% {
-      background-color: rgb(255, 255, 255);
-    }
-    50% {
-      background-color: rgb(0, 0, 0);
-    }
-    99% {
-      background-color: rgb(0, 0, 0);
-    }
-    100% {
-      background-color: rgb(255, 255, 255);
+  .contact-text {
+    font-size: 19px;
+    text-align: center;
+    color: white;
+
+    &__link {
+      color: yellow;
     }
   }
 </style>
---
+++ src/widgets/fade-menu-block.svelte
@@ -0,0 +1,105 @@
+<script>
+  import arrowRight from "~/shared/icons/arrow-right.svg";
+  import { link } from "svelte-spa-router";
+  import { fade } from "svelte/transition";
+</script>
+
+<div class="fade-block">
+  <h1 class="fade-block__title" in:fade={{ delay: 0, duration: 2000 }}>
+    <span class="wall-text"><b class="wall-text__aczhent">PIC</b>NICKER</span>
+  </h1>
+  <p class="fade-block__text" in:fade={{ delay: 2000, duration: 2000 }}>
+    <span class="sub-text"
+      >Offline tool to create your own mini business card!</span
+    >
+  </p>
+  <p class="fade-block__text" in:fade={{ delay: 4000, duration: 2000 }}>
+    <a class="geolink" href="/sandbox" use:link>
+      Let's start! <img
+        class="geolink__img"
+        src={arrowRight}
+        alt="right arrow"
+      />
+    </a>
+  </p>
+</div>
+
+<style lang="scss">
+  .fade-block {
+    height: 100%;
+    color: white;
+    font-size: 28px;
+    text-align: center;
+    padding: 5px 12px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    &__title {
+      width: 100%;
+      text-align: center;
+      font-size: 3em;
+      margin: 20px 0;
+    }
+
+    &__text {
+      font-size: 3rem;
+      padding: 0 3%;
+      margin: 20px 0;
+    }
+  }
+
+  .wall-text {
+    font-size: 7rem;
+    &__aczhent {
+      color: #d93d04;
+      font-size: 1.1em;
+    }
+  }
+
+  .sub-text {
+    font-size: 2.1rem;
+    font-weight: 100;
+  }
+
+  .geolink {
+    display: block;
+    padding: 10px 16px;
+    border: 2px solid white;
+    border-radius: 120px;
+    color: white;
+    font-size: 2.5rem;
+    line-height: 3rem;
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-weight: 100;
+    transition: gap 0.5s;
+
+    &:hover {
+      gap: 30px;
+    }
+
+    &__img {
+      height: 2.5rem;
+      filter: invert(1);
+    }
+  }
+
+  @media (max-width: 720px) {
+    .wall-text {
+      font-size: 4rem;
+    }
+
+    .sub-text {
+      font-size: 1.5rem;
+    }
+
+    .geolink {
+      font-size: 1.8rem;
+      line-height: 2rem;
+    }
+  }
+</style>
